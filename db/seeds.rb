3f78b75932258fee1ca9aae92fbb9dd37390c79a
Pranav Aher <pranav.aher@beyondwalls.com>
2024-01-30 18:38:22 +0530

    Setup devise authentication..

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+user = User.where(email: "user1@gmail.com").first_or_initialize
+user.update!(password: "pass123", password_confirmation: "pass123")
