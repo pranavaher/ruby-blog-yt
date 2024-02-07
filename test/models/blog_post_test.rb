c0785dadf55e7b61f5ee3f6989181d4d33f908e8
PRANAV AHER <prranavyaher@gmail.com>
2024-02-07 16:14:53 +0530

    Add test cases for blog posts..

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft? returns true for draft blog post" do
+    assert draft_blog_post.draft?
+  end
+
+  test "draft? returns false for published blog post" do
+    refute blog_posts(:published).draft?
+  end
+
+  test "draft? returns false for scheduled blog post" do
+    refute blog_posts(:scheduled).draft?
+  end
+
+  test "published? returns true for published blog post" do
+    assert blog_posts(:published).published?
+  end
+
+  test "published? returns false for draft blog post" do
+    refute draft_blog_post.published?
+  end
+
+  test "published? returns false for scheduled blog post" do
+    refute blog_posts(:scheduled).published?
+  end
+  
+  test "scheduled? returns true for scheduled blog post" do
+    assert blog_posts(:scheduled).scheduled?
+  end
+
+  test "scheduled? returns false for draft blog post" do
+    refute draft_blog_post.scheduled?
+  end
+
+  test "scheduled? returns false for published blog post" do
+    refute blog_posts(:published).scheduled?
+  end
+
+  def draft_blog_post
+    BlogPost.new(published_at: nil)
+  end
 end
